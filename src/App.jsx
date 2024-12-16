660ec2dc774c3333163a844ed282f5b4e0b08738
Aleks <karaeeeani@gmail.com>
2024-12-16 12:58:13 +0200

    Create pagination and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
-import Login from "./Login.jsx";
-import LoginError from "./LoginError.jsx";
+import Login from "./pages/Login.jsx";
+import LoginError from "./pages/LoginError.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Tarif from "./pages/Tarif.jsx";
+import MainPage from "./pages/MainPage.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />}></Route>
+        <Route path="/auth" element={<Login />}></Route>
+        <Route path="/authError" element={<LoginError />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/tarif" element={<Tarif />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
