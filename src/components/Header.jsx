41aa907beca955d002ed267a7fc6ecbda536ad1c
Aleks <karaeeeani@gmail.com>
2024-12-17 13:41:26 +0200

    Header.jsx (preliminary version- still need to fix when the designs are ready)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,34 +2,44 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="">
-      <div>
-        <div>ICON</div>
-        <div>
+    <div className="header-container">
+      <div className="header-first-line">
+        <div className="icon-par">
           <div>
-            <p>DL CHat</p>
+            <img className="pink-logo" src="../public/dlchat.png" alt="" />
           </div>
+
           <div>
-            <p>Admin</p>
+            <div>
+              <p>DL CHAT</p>
+            </div>
+            <div>
+              <p>Admin</p>
+            </div>
           </div>
+        </div>
+
+        <div className="users-par-div">
+          <p className="users-par">Пользователи</p>
         </div>
       </div>
 
-      <div class="underline">Пользователи</div>
-
-      <div>
+      <div className="buttons-container">
         <button className="search-button">Поиск</button>
 
-        <button>Добавить</button>
+        <button className="add-button">Добавить</button>
 
-        <div class="round-button-container">
-          <button class="button-first">««</button>
-          <button class="button-next">«</button>
-          <button class="button-middle">10V</button>
-          <button class="button-next">»</button>
-          <button class="button-last">»»</button>
+        <div class="five-btns">
+          <button class="first-btn">««</button>
+          <button class="second-btn">«</button>
+          <button class="third-btn">10V</button>
+          <button class="fourth-btn">»</button>
+          <button class="fifth-btn">»»</button>
         </div>
+
+        <button className="edit-button">Редактировать</button>
       </div>
+      <div className="header-underline"></div>
     </div>
   );
 };
